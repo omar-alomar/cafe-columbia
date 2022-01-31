a37abf048317669f4eec4c7ebd75a222f95d4536
Omar Alomar <omaro2000@gmail.com>
2022-01-31 13:19:01 -0500

    menu

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import Menu1 from './../../assets/img/menu1.png';
-import Menu2 from './../../assets/img/menu2.png';
+import Menu1 from './../../assets/img/menuColored.png';
+import MenuPage1 from './../../assets/img/menuCut1.png';
+import MenuPage2 from './../../assets/img/menuCut2.png';
 
 export default function Menu() {
   return (
     <>
     <div className="menu">
-      <img className="menu__img" src={Menu1} alt="" />
-      {/* <img className="menu__img" src={Menu2} alt="" /> */}
+      <h1>Our Menu</h1>
+      <div className="menu__img-container">
+        <img className="menu__img" src={MenuPage1} alt="" />
+        <img className="menu__img" src={MenuPage2} alt="" />
+      </div>
     </div>
     </>
   );
